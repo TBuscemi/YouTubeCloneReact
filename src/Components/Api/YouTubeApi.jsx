d1c65620acf1676871e44515f69a2141dc3ceb20
spencerjamesdev <spencer.franke@gmail.com>
2021-08-20 15:02:03 -0500

    Added Spencer's API key, but commented it out so have available when needed

--- src/Components/Api/YouTubeApi.jsx
+++ src/Components/Api/YouTubeApi.jsx
@@ -1,6 +1,7 @@
 import axios from "axios";
 
 const KEY= AIzaSyCYaSiKtKMiU-P91FpjJZJ5crSaBuMczVw
+//spencer's API key 'AIzaSyDs-up5MDmxBJK_Rrkw_l8sicAkHOgYpk4'
 
 export default axios.create({
     baseURL: 'https://www.googleapis.com/youtube/vs/',
